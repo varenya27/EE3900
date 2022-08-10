becc7b10d1b14c414dfc46f887019d1f427c3ffa
Varenya Upadhyaya <varenyau@gmail.com>
2022-08-10 14:34:12 +0530

    4.5 figure

--- Assignment-1/codes/3.py
+++ Assignment-1/codes/3.py
@@ -19,7 +19,12 @@ def H(z):
 omega = np.linspace(-3*np.pi,3*np.pi,100)
 
 #subplots
+dx=0.2
 plt.plot(omega, abs(H(np.exp(1j*omega))))
+plt.plot(0,4/3,marker='o', color= 'black')
+plt.plot(2*np.pi,4/3,marker='o',color='black')
+plt.text(0+dx,4/3, "0,1.33")
+plt.text(2*np.pi+dx,4/3, "2$\pi$,1.33")
 plt.title('Filter Frequency Response')
 plt.xlabel('$\omega$')
 plt.ylabel('$|H(e^{\jmath\omega})| $')

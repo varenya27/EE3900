e09cf2ef839154293cae0dfb151de4fed2e8211e
Varenya Upadhyaya <varenyau@gmail.com>
2022-10-27 09:34:53 +0530

    charger

---
+++ charger/codes/1_1.py
@@ -0,0 +1,12 @@
+import numpy as np 
+from matplotlib import pyplot as plt 
+import math
+A,f = 12,50
+t = np.arange(-0.025,0.025,0.001)
+print(np.sin(2*np.pi))
+plt.figure() 
+plt.plot(t,A*abs(np.sin( 2*(np.pi)*f*t) ))
+plt.grid()
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.savefig('../figures/1_1.png')
